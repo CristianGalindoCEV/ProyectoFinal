8dcd296780e5e3486918fab001b502ecf3f66e99
Chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2020-11-10 01:47:40 +0100

    Mejora del MainMenu + canvas pause

---
+++ Anoras Quest/Assets/Scripts/CanvasAndButons/HUD & Cam/InputManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+   //Armas
+    [SerializeField] private GameObject Bauculo;
+    [SerializeField] private GameObject Espada;
+    public bool BauculoItem = false;
+    public bool EspadaItem = true;
+   
+    //HUD
+    public GameObject hud;
+    public GameObject ingamemenu;
+    public GameObject mirilla;
+    public bool menuon;
+    public GameMaster gamemaster;
+
+    //Player
+    private PlayerController player;
+
+    //Cursor
+    private bool m_islocked;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bauculo.SetActive(false);
+        player = FindObjectOfType<PlayerController>();
+        menuon = false;
+        ingamemenu.SetActive(false);
+
+        //Cursor
+        Cursor.visible = (false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Combate y armas
+        if (BauculoItem == true)
+        {
+            Espada.SetActive(false);
+            Bauculo.SetActive(true);
+            EspadaItem = (false);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Bauculo.Fire();
+            }
+        }
+        
+        else if (EspadaItem == true)
+        {
+            Espada.SetActive(true);
+            Bauculo.SetActive(false);
+            BauculoItem = (false);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Espada.Fire();
+            }
+        }
+        
+        if (Input.GetKey("1"))
+        {
+            EspadaItem = true;
+            BauculoItem = false;
+            mirilla.SetActive(false);
+        }
+        
+        if (Input.GetKey("2") && gamemaster.unlocked == true)
+         {
+            BauculoItem = true;
+            EspadaItem = false;
+            mirilla.SetActive(true);
+         }
+        //Salto
+        //player.SetAxis(Input.GetAxis("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+            player.Jump();
+
+        //Canvas
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if( menuon == false)
+            {
+                ingamemenu.SetActive(true);
+                menuon = true;
+            }else
+            {
+                ingamemenu.SetActive(false);
+                menuon = false;
+            }
+        }
+
+        //Cursor
+        if (menuon == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = (true);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = (false);
+        }
+        
+    }
+
+
+}
---
+++ Anoras Quest/Assets/Scripts/CanvasAndButons/MainMenuManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public GameObject panelresolution;
+    public GameObject panelsound;
+    public GameObject panelgraphics;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        panelgraphics.SetActive(false);
+        panelresolution.SetActive(false);
+        panelsound.SetActive(false);
+
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i= 0; i< resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions [i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    void Update()
+    {
+       
+    }
+
+    //Botones de los settings
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    //Botones del menu Ingame
+    public void PulsaResolution()
+    {
+        panelgraphics.SetActive(false);
+        panelresolution.SetActive(true);
+        panelsound.SetActive(false);
+    }
+
+    public void PulsaSound()
+    {
+        panelgraphics.SetActive(false);
+        panelresolution.SetActive(false);
+        panelsound.SetActive(true);
+    }
+
+    public void PulsaGraphics()
+    {
+        panelgraphics.SetActive(true);
+        panelresolution.SetActive(false);
+        panelsound.SetActive(false);
+    }
+}
+
